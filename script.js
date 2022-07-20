bfbe8cb5e63e2409e429773292fc16e2a136c7a1
Kent <kenttechnical@gmail.com>
2022-07-20 14:41:55 +0200

    Filter Method and Map Method added

--- script.js
+++ script.js
@@ -26,3 +26,27 @@ let premiumUsers = users.filter((user)=>{
 console.log(premiumUsers)
 
 // #2 - Map
+// Map creates a new array based off another array, it's great for creating a new array based off another by manipulating its values
+// Avoid directly altering the variable and use return statement to avoid destruction of the original variable
+
+// Creates a new sales price by increasing each number by /100 and *114, thus increasing the price by 14% (May be inaccurate maths)
+
+const prices = [25,10,57,42,50,39,100]
+let salesPrice = prices.map((price)=>{
+    return price /100 *114
+})
+
+console.log(salesPrice)
+
+
+// Halves the price of menu items over the price of 30 and stores in menuSale array object
+
+const menu = [{name:"Spaghetti",price:50},{name:"Pizza",price:70},{name:"Noodles",price:30},{name:"Tea",price:12}]
+
+menuSale = menu.map((item)=>{
+    if(item.price>30){
+        return {name:item.name,price:item.price/2}
+    } else return item
+})
+
+console.log(menuSale)
